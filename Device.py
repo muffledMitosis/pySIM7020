1defdab21129ac37d53fec967b2cf52680646338
Meth Munindradasa <methsndr@gmail.com>
2025-03-13 10:22:40 +0000

    Add some getters

--- Device.py
+++ Device.py
@@ -4,11 +4,18 @@ import logging
 import sys
 from dataclasses import dataclass
 from typing import Optional
+from typing import List
 
 @dataclass
 class UART_OPS:
   port: str
   baud: int
+
+@dataclass
+class ATResponse:
+  echo: str
+  response: List[str]
+  success: bool
 
 class Device:
   def __init__(self, uartOps: Optional[UART_OPS] = None):
@@ -24,4 +31,32 @@ class Device:
       sys.exit()
 
   def __sendAT(self, command: str):
-    self.__serial.
+    self.__serial.write(f"{command}\r\n".encode())
+    self.__serial.flush()
+
+    response_lines = [line.decode().strip() for line in self.__serial.readlines()]
+
+    echo = response_lines[0] if response_lines else ""
+    success = response_lines[-1] == "OK" if response_lines else False
+    response = response_lines[1:-1] if success else response_lines[1:]
+
+    return ATResponse(echo=echo, response=response, success=success)
+
+
+  def getSignalQuality(self) -> ATResponse:
+    return self.__sendAT("AT+CSQ")
+
+  def getFirmwareVersion(self) -> ATResponse:
+    return self.__sendAT("AT+CGMR")
+
+  def getInternetRegisterStatus(self) -> ATResponse:
+    return self.__sendAT("AT+CGREG?")
+
+  def getPDPStatus(self) -> ATResponse:
+    return self.__sendAT("AT+CGACT?")
+
+  def getInternetInfo(self) -> ATResponse:
+    return self.__sendAT("AT+COPS?")
+
+  def getInternetStatus(self) -> ATResponse:
+    return self.__sendAT("AT+CGCONTRDP")
